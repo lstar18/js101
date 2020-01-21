a1add8624ffcb3fa69f92b15a132a9e30ac9cf72
Liza Star <lizadstar18@gmail.com>
2020-01-20 20:44:29 -0600

    finished object challenge

--- objects.js
+++ objects.js
@@ -19,3 +19,45 @@ const ageCheck = (age) => {
 ageCheck(12);
 ageCheck(24);
 
+const expense = {
+    dateCreated: '01/17/2020',
+    location: 'Baja Burrito',
+    cost: 12.57,
+    isGood: true
+};
+// .dot notations-always use
+console.log(expense.cost)
+//bracket notation
+console.log(expense['cost'])
+
+const employee = {
+    id: '1234',
+    firstName: 'Liza',
+    lastName: 'Star',
+    startDate: 'January 1st 2020',
+    title: 'Teacher',
+    gradeLevel: "4th",
+    collegeDegree: true,
+    age: 30,
+    supervisor: '2345'
+};
+console.log(employee.firstName);
+//created function called statusMaker that takes simgle meployy
+//if employee firstname starts with an M then add key of status vip
+//if employee firstname doesnt add key of status peaseant
+//return employy
+const statusMaker = (emp) => {
+    if (emp.firstName.startsWith('M')) {
+        emp.status = 'vip';
+    }
+    
+    else {
+        emp.status = 'peasant';
+    
+    }
+    return emp;
+}
+
+console.log(statusMaker(employee));
+
+
