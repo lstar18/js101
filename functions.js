52d3940fade0310cc9273d09f94a88ab9a940854
Liza Star <lizadstar18@gmail.com>
2020-01-20 18:47:58 -0600

    wrtoe nuggetizer function and added script tag

---
+++ functions.js
@@ -0,0 +1,13 @@
+console.log("FUNCTIONS");
+const nuggetizer = (animal) => {
+const output = `processed ${animal}`;
+return output;
+//processed fish
+
+};
+
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('cow'));
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('fish'));
+
