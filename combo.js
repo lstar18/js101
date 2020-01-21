2ba7fb2a8003d275d22526cc483a3c8311137485
Liza Star <lizadstar18@gmail.com>
2020-01-20 21:31:01 -0600

    combo challenge

---
+++ combo.js
@@ -0,0 +1,29 @@
+console.log('COMBO');
+
+const evenOdd = (num) => {
+    if (num % 2===0) {
+    return ('Even')
+    }
+    else {
+    return ('Odd')
+    }
+
+};
+console.log(evenOdd(6));
+console.log(evenOdd(10));
+console.log(evenOdd(17));
+
+
+
+const oppositeNum = (anyNum) => {
+ return anyNum * -1
+};
+console.log(oppositeNum(500));
+
+const palindrome = () => {
+
+};
+
+const frogLawernece = () => {
+
+};
